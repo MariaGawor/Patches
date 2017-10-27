0ab6264f3829ef07b7174b27b512dcdc440586ea
GaworO <ola.gawor@gmail.com>
2017-10-27 18:14:05 +0200

    upadte

---
+++ src/main/java/com/teaseme/teaseme/TeasemeApplication.java
@@ -0,0 +1,12 @@
+package com.teaseme.teaseme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeasemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeasemeApplication.class, args);
+	}
+}
